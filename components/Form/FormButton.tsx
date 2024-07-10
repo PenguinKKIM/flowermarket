ac3f8eca6522700872492a4e855631224476ac3d
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-07-10 22:24:07 +0900

    Add : CVA 속성 추가

--- components/Form/FormButton.tsx
+++ components/Form/FormButton.tsx
@@ -1,21 +1,37 @@
 'use client';
 
+import { cva } from 'class-variance-authority';
 import { useFormStatus } from 'react-dom';
+
+type Intent = 'primary' | 'disable';
 
 interface FormButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   text: string;
+  intent?: Intent;
 }
 
-function FormButton({ text, type, ...props }: FormButtonProps) {
+const buttonStyleCVA = cva(
+  'p-4 rounded-lg bg-blue-400 text-white hover:bg-blue-600 disabled:bg-slate-500',
+  {
+    variants: {
+      intent: {
+        primary: 'bg-blue-400',
+        disable: 'bg-gray-100',
+      },
+    },
+  },
+);
+
+function FormButton({ text, type, intent, ...props }: FormButtonProps) {
   const { pending } = useFormStatus();
   return (
     <button
       disabled={pending}
       type={type}
-      className="p-4 rounded-lg bg-blue-400 text-white hover:bg-blue-600 disabled:bg-slate-500"
+      className={buttonStyleCVA({ intent })}
       {...props}
     >
-      {pending ? '로그인 중' : text}
+      {pending ? '잠시 기다려주세요' : text}
     </button>
   );
 }
